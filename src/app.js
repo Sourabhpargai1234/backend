1843bef455fb892fbea0adf5a94caefbca5061b0
Sourabh <Sourabhpar@gmail.com>
2024-05-26 22:32:30 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import "dotenv"
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -8,10 +9,16 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//declaration
+app.use("/api/v1/users", userRouter)
+
 export { app }
