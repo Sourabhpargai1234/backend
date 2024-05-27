4c476d6f03c2eda65ac63ae96d366177081b07a9
Sourabh <Sourabhpar@gmail.com>
2024-05-27 10:05:09 +0530

    done

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -33,7 +33,11 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new ApiError(409, "User existed already with username or email")
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files?.coverImage[0]?.path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required :", avatarLocalPath)
